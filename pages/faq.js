8223721d4ef2e3d6430527f94943eb38b5b8e43e
Elias de Souza <elias99-souza@hotmail.com>
2022-11-11 08:38:36 -0300

    'projetoNextjs'

---
+++ pages/faq.js
@@ -0,0 +1,13 @@
+import Link from '../src/components/Link'
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>FAQ Page</h1>
+
+            <Link href='/'>
+                Voltar para Home
+            </Link>
+        </div>
+    )
+}
